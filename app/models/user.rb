de7dc068607e9f3b9643013025c02d8167986c7f
Chris Crane <chrisjcrane@gmail.com>
2023-04-12 13:18:18 -0600

    Add validations for User class
    Co-authored-by: Conner <cdvanloan@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates_presence_of :username
+  validates_uniqueness_of :email
+  validates_presence_of :password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of :username }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :password }
+  end
+
+  xcontext "relationships" do
+    it { should have_many :day_plans}
+    it { should have_many :day_plan_recipes, through: :day_plans}
+    it { should have_many :recipes, through: :day_plan_recipes}
+  end
 end
