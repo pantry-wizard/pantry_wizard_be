815e39ce1304c2ca6a099604cdcb6a434cfa1f1d
Conner Van Loan <cdvanloan@gmail.com>
2023-04-13 12:24:31 -0600

    updates models, tests, to ensure primary/foreign key function

--- db/migrate/20230412192329_create_day_plans.rb
+++ db/migrate/20230412192329_create_day_plans.rb
@@ -1,10 +1,11 @@
 class CreateDayPlans < ActiveRecord::Migration[7.0]
   def change
     create_table :day_plans do |t|
-      t.references :user, foreign_key: { primary_key: :google_id}, type: :string
+      t.references :user, foreign_key: { primary_key: :google_id }, type: :string
       t.date :date
       t.string :meal_type
-      
+      t.string :google_id
+
       t.timestamps
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "validations" do
+    let(:google_id) { '12aB4567890' }
+    
+    before do
+      # create a user with a valid google_id
+      User.create(email: 'test@example.com', google_id: google_id, name: 'test')
+    end
+
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
     it { should validate_uniqueness_of :email }
