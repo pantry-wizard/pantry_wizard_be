49ae5a26295ac63d04999ba2861f39ee937877d6
Conner Van Loan <cdvanloan@gmail.com>
2023-04-14 15:19:52 -0600

    adds sad pathing for user end point testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show, :create, :update, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy] do
+        resources :day_plans, only: [:index, :show, :create, :update, :destroy]
+      end
     end
   end
 end
